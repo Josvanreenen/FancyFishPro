7ef8bfcf246dd63899a68f65395b745184ed0906
Jos van Reenen <jos.vanreenen@hu.nl>
2021-03-19 11:10:02 +0100

    please do not forget Spring hierarchy

--- src/main/java/nl/hu/bep/example/fancyfishpro/controller/InitialController.java
+++ src/main/java/nl/hu/bep/example/fancyfishpro/controller/InitialController.java
@@ -1,4 +1,4 @@
-package nl.hu.bep.example.controller;
+package nl.hu.bep.example.fancyfishpro.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class InitialController {
 
-    @GetMapping ( "/hello")
-    public String firstPage(){
+    @GetMapping("/hello")
+    public String answerMe(){
         return "Hello FancyFishPro World!";
     }
 }
