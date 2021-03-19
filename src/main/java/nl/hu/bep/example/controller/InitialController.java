144e5ef7b3bce83ead8fe7e2820ec41dfef51f52
Jos van Reenen <jos.vanreenen@hu.nl>
2021-03-19 10:45:24 +0100

    removed {}

--- src/main/java/nl/hu/bep/example/controller/InitialController.java
+++ src/main/java/nl/hu/bep/example/controller/InitialController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class InitialController {
 
-    @GetMapping ({ "/hello"})
+    @GetMapping ( "/hello")
     public String firstPage(){
         return "Hello FancyFishPro World!";
     }
